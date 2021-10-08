5106d2841527fd5065862b79d1a3e35264698097
bluephosphor <66392799+bluephosphor@users.noreply.github.com>
2021-10-07 21:36:04 -0400

    cleaning

--- script/index.js
+++ script/index.js
@@ -1,5 +1,4 @@
-const intro = 
-` # niko.
+const intro = ` # niko.
 
 Welcome to my site! My name is Jonny (my friends call me Niko) and I'm currently self-studying to become a web-developer! This is where I'll be hosting portfolio projects, gamedev and music related things, blog posts and more. 
 
--- script/root.js
+++ script/root.js
@@ -1,3 +1,7 @@
+// Some small essentials. 
+const id = (id) => document.getElementById(id);
+const irandom_range = (min, max) => Math.floor(Math.random() * (max - min) ) + min;
+
 // Keeping main site data in one place.
 const Site = {
   nav:[
@@ -9,21 +13,18 @@ const Site = {
   ]
 }
 // Building the main navbar for every page.
-const id = (id) => document.getElementById(id);
 id('nav-main').innerHTML += `<img class="img-round" src="asset/phosphora.png">`;
+
 Site.nav.forEach(elem => id("nav-main").innerHTML += `<a href="${elem.url}">${elem.name}</a>`);
 
 // Some function declarations.
-
-const irandom_range = (min, max) => Math.floor(Math.random() * (max - min) ) + min;
-
-function generate_paras(num, sentenceMin, sentenceMax, tag){
+function generate_paras(num, sentence_min, sentence_max, tag){
   let words = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum".toLowerCase().split(" ");
   let str = "", i = 0, j = 0;
   
   while (i < num){
     str += `<${tag}>`; 
-    j = 0; while (j < irandom_range(sentenceMin, sentenceMax)){
+    j = 0; while (j < irandom_range(sentence_min, sentence_max)){
       str += words[irandom_range(0,words.length)] + " ";
       j++;
     }
