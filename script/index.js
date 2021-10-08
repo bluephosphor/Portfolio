d6dcf223f8f8fa7398d862466098bf2a97e0141a
Niko <bow682@gmail.com>
2021-10-07 21:02:10 -0400

    Update index.js

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ const intro =
 
 Welcome to my site! My name is Jonny (my friends call me Niko) and I'm currently self-studying to become a web-developer! This is where I'll be hosting portfolio projects, gamedev and music related things, blog posts and more. 
 
-Thanks for stopping by! :)`;
+Thanks for stopping by! :3`;
 
 id('content').innerHTML = marked(intro);
 id('nav-secondary').innerHTML += `<img class="img-round" src="asset/fallshrubbie.png">`;
